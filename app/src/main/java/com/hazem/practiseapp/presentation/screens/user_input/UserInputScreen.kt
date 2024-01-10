111ad9a7e2761713cd60bfb01017bb65530c3af3
hazem khaled <hazemk778@gmail.com>
2024-01-10 18:57:28 +0200

    remove unnecessary imports

--- app/src/main/java/com/hazem/practiseapp/presentation/screens/user_input/UserInputScreen.kt
+++ app/src/main/java/com/hazem/practiseapp/presentation/screens/user_input/UserInputScreen.kt
@@ -1,6 +1,5 @@
 package com.hazem.practiseapp.presentation.screens.user_input
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
